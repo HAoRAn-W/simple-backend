274d6adb2b922b5b405c8d9124491185993d6700
HAoRAn-W <haoran2326@gmail.com>
2022-12-27 17:24:33 -0500

    add repositories

--- src/main/java/one/whr/simple/repository/CommentRepository.java
+++ src/main/java/one/whr/simple/repository/CommentRepository.java
@@ -1,9 +1,11 @@
 package one.whr.simple.repository;
 
 import one.whr.simple.entity.Comment;
+import one.whr.simple.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 }
--- src/main/java/one/whr/simple/repository/PostRepository.java
+++ src/main/java/one/whr/simple/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package one.whr.simple.repository;
 
+import one.whr.simple.entity.Comment;
 import one.whr.simple.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
