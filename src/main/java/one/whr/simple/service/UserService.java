d52a47e4fed53a942e4f9d3b4a4f5b2140571dc2
HAoRAn-W <haoran2326@gmail.com>
2022-12-28 14:20:57 -0500

    remove unused methods in services

--- src/main/java/one/whr/simple/service/UserService.java
+++ src/main/java/one/whr/simple/service/UserService.java
@@ -1,9 +1,7 @@
 package one.whr.simple.service;
 
-import one.whr.simple.entity.User;
 import one.whr.simple.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +9,4 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username is not found by username: " + username));
-    }
 }
