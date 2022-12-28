32069617b6a6ed9f6ed46ac6fdee4134f73e2b83
HAoRAn-W <haoran2326@gmail.com>
2022-12-28 17:58:38 -0500

    optimized imports

--- src/main/java/one/whr/simple/repository/PostRepository.java
+++ src/main/java/one/whr/simple/repository/PostRepository.java
@@ -1,11 +1,8 @@
 package one.whr.simple.repository;
 
-import one.whr.simple.entity.Comment;
 import one.whr.simple.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
