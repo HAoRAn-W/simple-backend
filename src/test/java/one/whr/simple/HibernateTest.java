e223eb92eab4295391c78e0ef8780b3fc19c4cb7
HAoRAn-W <haoran2326@gmail.com>
2022-12-29 16:20:16 -0500

    test updated

--- src/test/java/one/whr/simple/HibernateTest.java
+++ src/test/java/one/whr/simple/HibernateTest.java
@@ -1,18 +1,13 @@
 package one.whr.simple;
 
 import one.whr.simple.entity.Comment;
-import one.whr.simple.entity.Post;
 import one.whr.simple.entity.User;
 import one.whr.simple.repository.CommentRepository;
-import one.whr.simple.repository.PostRepository;
 import one.whr.simple.service.RoleService;
 import one.whr.simple.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.transaction.Transactional;
-import java.util.List;
 
 @SpringBootTest
 public class HibernateTest {
@@ -22,22 +17,13 @@ public class HibernateTest {
     @Autowired
     UserService userService;
     @Autowired
-    private PostRepository postRepository;
-    @Autowired
     private CommentRepository commentRepository;
 
 
 
 
 
-    @Test
-    @Transactional
-    public void postGetComments() {
-        Post post = postRepository.getById(1L);
-        List<Comment> list = post.getComments();
-        System.out.println(list);
-        assert list.size() == 3;
-    }
+
 
     @Test
     public void commentGetUser() {
