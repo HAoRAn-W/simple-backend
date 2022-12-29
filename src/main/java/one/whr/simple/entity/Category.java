baf259cf5c45b55fe444ece1ac449a6771ae19f0
HAoRAn-W <haoran2326@gmail.com>
2022-12-28 20:36:39 -0500

    fix typo: ID is Long,not String

--- src/main/java/one/whr/simple/entity/Category.java
+++ src/main/java/one/whr/simple/entity/Category.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class Category {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cate_id")
-    private String categoryId;
+    private Long categoryId;
 
     @Column(name = "cate_name")
     private String categoryName;
