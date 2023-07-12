c3f611ccc6870da7521687b8ed123eff9d4ff2df
Haoran Wang <haoran2326@gmail.com>
2023-07-11 22:58:45 -0400

    add refresh token

--- src/main/java/one/whr/simple/exceptions/UserNotFoundException.java
+++ src/main/java/one/whr/simple/exceptions/UserNotFoundException.java
@@ -1,7 +1,5 @@
 package one.whr.simple.exceptions;
 
-import one.whr.simple.entity.User;
-
 public class UserNotFoundException extends Exception{
     private String message;
     public UserNotFoundException(String message) {
@@ -13,7 +11,4 @@ public class UserNotFoundException extends Exception{
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
