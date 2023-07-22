495066454553abe6e49178fbb51c68fcadf24f73
Haoran Wang <haoran2326@gmail.com>
2023-07-21 21:26:16 -0400

    modified test controller

--- src/main/java/one/whr/simple/controller/TestController.java
+++ src/main/java/one/whr/simple/controller/TestController.java
@@ -1,5 +1,8 @@
 package one.whr.simple.controller;
 
+import one.whr.simple.constant.MessageCode;
+import one.whr.simple.dto.response.MessageResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,19 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
     @GetMapping("/all")
-    public String allAccess() {
-        return "Public Content.";
+    public ResponseEntity<?> allAccess() {
+        return ResponseEntity.ok().body(new MessageResponse(MessageCode.SUCCESSFUL, "public content"));
     }
 
     @GetMapping("/user")
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-    public String userAccess() {
-        return "User Content.";
+    public ResponseEntity<?>  userAccess() {
+        return ResponseEntity.ok().body(new MessageResponse(MessageCode.SUCCESSFUL, "user content"));
     }
 
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public String adminAccess() {
-        return "Admin Board.";
+    public ResponseEntity<?>  adminAccess() {
+        return ResponseEntity.ok().body(new MessageResponse(MessageCode.SUCCESSFUL, "admin content"));
     }
 }
