418ea0330f4060c225fd0cdeb1f2d44dafa73146
Haoran Wang <ryan26us@gmail.com>
2023-08-29 22:32:10 -0400

    minor fix

--- src/main/java/one/whr/simple/entity/Avatar.java
+++ src/main/java/one/whr/simple/entity/Avatar.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Avatar {
@@ -12,10 +12,10 @@ public class Avatar {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotBlank
     private String name;
 
-    @NotNull
+    @NotBlank
     private String url;
 
     public Avatar(String name, String url) {
