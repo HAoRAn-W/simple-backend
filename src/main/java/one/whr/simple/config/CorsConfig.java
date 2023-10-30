fd225ed204c44a3d47efc2e2488ca99008ef352a
Haoran Wang <ryan26us@gmail.com>
2023-10-30 14:50:03 -0400

    eliminate @EnableWebMVC, this will cause LocalDateTime format into an int array.https://stackoverflow.com/a/63698586

--- src/main/java/one/whr/simple/config/CorsConfig.java
+++ src/main/java/one/whr/simple/config/CorsConfig.java
@@ -3,11 +3,10 @@ package one.whr.simple.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Value("${whr.app.allowOrigins}")
     private String origins;
